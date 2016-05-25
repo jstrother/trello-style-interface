2ef4ed3c1911d113c717e294306bbb544e0e8e48
Jim Strother <jim.strother@gmail.com>
2016-05-24 21:11:34 -0700

    understanding better how props work

--- index.jsx
+++ index.jsx
@@ -1,49 +1,61 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Card = function() {
-	var title = "A Card for Stuff";
-	var body = "The things we are writing go here!";
+var Card = function(props) {
 	return (
 		<div className="card">
-			<div className="card-title">{title}</div>
+			<div className="card-title">{props.title}</div>
+			<div className="card-body">{props.body}</div>
 			<hr />
-			<div className="card-body">{body}</div>
 		</div>
 	);
 };
 
-var List = React.createClass({
-	render: function() {
-		var cards = [];
-		for (var i = 0; i < 5; i++) {
-			cards.push(<Card key={i}/>);
-			cards.push(<br key={i + 10}/>);
-		}
-		return (
-			<div className="list">
-				{cards}
-			</div>
-		);
-	}
-});
+var List = function(props) {
+	return (
+		<div className="list">
+			<div className="list-title">{props.title}</div>
+			<br />
+			<hr />
+			<br />
+		</div>
+	);
+};
 
-var Board = React.createClass({
-	render: function() {
-		var lists = [];
-		for (var i = 0; i < 5; i++) {
-			lists.push(<List key={i}/>);
-			lists.push(<br key={i + 20}/>);
-			lists.push(<br key={i + 30}/>);
-			lists.push(<br key={i + 40}/>);
-		}
-		return (
-			<div className="list">
-				{lists}
-			</div>
-		);
-	}
-});
+var Board = function() {
+	return (
+		<div className="board">
+			<List title="Seattle Bands" />
+				<Card title="Nirvana"
+							body="Nevermind, In Bloom, Negative Creep" />
+				<Card title="Pearl Jam"
+							body="Alive, Jeremy, Elderly Woman Behind the Counter in a Small Town" />
+				<Card title="Soundgarden"
+							body="Spoonman, Black Hole Sun, Burden in My Hand" />
+			<List title="Not Seattle Bands" />
+				<Card title="Metallica"
+							body="Unforgiven, Seek and Destroy, Blackened" />
+				<Card title="The Who"
+							body="My Generation, Love Reign o'er Me, Behind Blue Eyes" />
+				<Card title="Led Zepplin"
+							body="Immigrant Song, Ramble On, Stairway to Heaven" />
+		</div>
+	);
+
+	// data: {
+	// 	title: "Favorite Bands",
+	// 	lists: [
+	// 		{
+	// 			title: "Seattle Bands",
+	// 			cards: ["Nirvana", "Pearl Jam", "Soundgarden"]
+	// 		},
+	// 		{
+	// 			title: "Not Seattle Bands",
+	// 			cards: ["Metallica", "The Who", "Led Zepplin"]
+	// 		}
+	// 	]
+	// }
+};
 
 document.addEventListener('DOMContentLoaded', function() {
 	ReactDOM.render(<Board />, document.getElementById('app'));
